503cb08044cbb3868f2dc0456ca5b070bc378187
jonatahessa <jonata.hessa@gmail.com>
2017-07-10 13:52:03 -0300

    First

---
+++ PizzariaDomenico/src/main/java/br/com/pizzariadomenico/Process/Adicionar.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.pizzariadomenico.Process;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author jonat
+ */
+@WebServlet(name = "Adicionar", urlPatterns = {"/Adicionar"})
+public class Adicionar extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession sessao = request.getSession();
+        if (sessao.getAttribute("logado") == null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/Entrar");
+            dispatcher.forward(request, response);
+        } else {
+            Produto pizza = new Produto();
+            if (request.getParameter("codigo").equalsIgnoreCase("")) {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/Manutencao");
+                dispatcher.forward(request, response);
+            }
+            pizza.setCodigo(Integer.parseInt(request.getParameter("codigo")));
+            pizza.setNome(request.getParameter("nome"));
+            pizza.setDescricao(request.getParameter("descricao"));
+            pizza.setPreco(request.getParameter("preco"));
+            pizza.setTipo(request.getParameter("tipo"));
+
+            try {
+                Utils.inserirPizza(pizza);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/Manutencao");
+                dispatcher.forward(request, response);
+            } catch (Exception ex) {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/Manutencao");
+                dispatcher.forward(request, response);
+            }
+        }
+    }
+}
